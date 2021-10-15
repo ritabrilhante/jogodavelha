6a649eaa2203ea727194cc864e05fd9409b39d3c
ritabrilhante <rita.brilhante@basement.io>
2021-10-15 15:30:44 -0300

    update code

--- README.md
+++ README.md
@@ -1,11 +1,7 @@
-# Projeto da Semana 1 e 2 
+# Projeto da Semana 1 e 2
 
 Jogo da velha desenvolvido em HTML, CSS e JAVASCRIPT.
 
-## Próximos passos: <br>
-- Adicionar Placar; 
-- Verificar Vencedor; 
-- Tela ou Modal de vencedor e empate; 
 
 
 
--- script.js
+++ script.js
@@ -1,125 +1,151 @@
-// Declarar os dois jogadores;
-// Declarar quem começa;
-// Fazer a função de trocar o jogador;
-// Mostrar no verificador de quem é a vez;
-// Marcar o field com a imagem correspondente;
-// Verificar linha horizontal, vertical e diagonal;
-// Se houver ganhador, mostrar a imagem do respectivo;
-// Se for empate, as duas imagens;
-// Armazenar vencedor em memória, mostrar no placar (até a página ser atualizada);
-
-
-// Jogadores
+//Global Variables
 const player1 = "xis";
 const player2 = "ball";
 
-//Default Start
-var turnOf = player1; //sempre vai começar;
-var gameOver = false;
+let round = 0;
+let player1Wins = 0;
+let player2Wins = 0;
 
-//Modal
-var btnRefresh = document.getElementById("restart");
+var turnOf = player1;
+var btnRefresh = document.getElementById("refresh");
 var winner = '';
 
-const winStates = [
+const winState = [
+    ['a1','a2','a3'],
+    ['b1','b2','b3'],
+    ['c1','c2','c3'],
 
-    //Posições
+    ['a1','b1','c1'],
+    ['a2','b2','c2'],
+    ['a3','b3','c3'],
 
-    [0, 1, 2], //horizontal
-    [3, 4, 5], //horizontal
-    [6, 7, 8], //horizontal
-    [0, 3, 6], //vertical
-    [1, 4, 7], //vertical
-    [2, 5, 8], //vertical
-    [0, 4, 8], //diagonal
-    [2, 4, 6]  //diagonal
+    ['a1','b2','c3'],
+    ['c1','b2','a3']
 ]
+//Clean fields;
+function setRefreshBtn() {
+    btnRefresh.addEventListener("click",function(){
+        const fieldByClass = [].slice.call(document.getElementsByClassName("field"))
 
-// Função para definir vez do jogador, e mostrar a respectiva imagem.
-wichPlayer()
+        fieldByClass.forEach(field => field.innerHTML = "");
+        document.getElementById("active").style.visibility = 'hidden';
 
-function wichPlayer() {
+        round = 0;
+    })
+}
+// Show wich player;
+showPlayerTurn()
+
+function showPlayerTurn() {
     const playerImage = document.getElementById("wich-player")
 
-    if (gameOver) return
-
-    if (turnOf == player1) {
-        playerImage.setAttribute("src", "imagens/xis.svg")
-    } else {
-        playerImage.setAttribute("src", "imagens/ball.svg")
-    }
+    playerImage.setAttribute("src", `imagens/${turnOf}.svg`);
 }
-
-//Função que mostra a imagem no campo clicado
-setFields();
 
 function setFields() {
     //Pegou todos os elementos com a classe "field" e atribuiu a um array
     const fieldsbyClass = document.getElementsByClassName("field")
-
-    console.log(fieldsbyClass)
 
     //Para todo field do array fieldByClass;
     for (let field of fieldsbyClass) {
 
         field.addEventListener("click", function(){
 
-            //Verifiquei se o field está vazio.
-            if (field.innerHTML != "") return
+            round++;
 
-            if (turnOf == player1) {
+            //Verifiquei se o field está vazio.
+            if (field.innerHTML !== "") return
+
+            if (turnOf === player1) {
                 field.innerHTML = "<img src='imagens/xis.svg'>"
-                //colocar o verifyWinner
-                turnOf = player2
+
+                const resultWinner = verifyWinner(player1,field)
+
+                //verify if there's not a winner, then a change the turnOf value.
+
+                if (!resultWinner) turnOf = player2;
             }else {
                 field.innerHTML = "<img src='imagens/ball.svg'>"
 
-                turnOf = player1
-            } wichPlayer()
+                const resultWinner = verifyWinner(player2,field)
+
+                //verify if there's not a winner, then a change the turnOf value.
+                if (!resultWinner) turnOf = player1;
+            }
+            if (round === 9) setWinner(null);
+
+            showPlayerTurn()
         })
     }
-    // verifyWinner()
 }
-
 //verificar quem ganhou
+function verifyWinner(player,field){
+    const fields = document.getElementsByClassName("field"); //get all fields from html;
+    const groupsToCheck = winState.filter(state => state.includes(field.id));
+    //.filter cria um novo array de arrays dos caso de vitória que possuem o id do campo clicado;
+    //.include verifica se o array possui um id, e retorna o array.
 
-function verifyWinner(field){
+    for(let group of groupsToCheck) {
+        const [id1,id2,id3] = group;
 
-    const fieldsbyClass = document.getElementsByClassName("field")
-
-    //[a1,a2,a3,b1,b2,b3,c1,c2,c3]
-
-    for(let winState of winStates) {
-        console.log(winState)
-
+        if (fields[id1].innerHTML === fields[id2].innerHTML && fields[id1].innerHTML === fields[id3].innerHTML) {
+            setWinner(player)
+            return true;
+        };
     }
 
+    return false;
 }
 
-    //Winner Modal
+function setWinner(player) {
+    const winnerImage = document.getElementById("winner-image")
 
+    let textModal= document.getElementById("text-modal")
 
-    // btnRefresh.addEventListener("click", function() {
-    //     location.reload();
-    // })
+    if (player === player1) {
+        winnerImage.setAttribute("src", "imagens/xis.svg")
 
-    whoWins ()
+        textModal.innerHTML = "O vencedor foi"
 
-    function whoWins() {
-        const winnerImage = document.getElementById("winner-image")
+        player1Wins++
 
-        if (winner === player1) {
-            winnerImage.setAttribute("src", "imagens/xis.svg")
-        } else if (winner === player2) {
-            winnerImage.setAttribute("src", "imagens/ball.svg")
-        } else {
-            let textModal = document.getElementById("text-modal")
+    } else if (player === player2) {
+        winnerImage.setAttribute("src", "imagens/ball.svg")
 
-            textModal.innerHTML = "Empatou"
+        textModal.innerHTML = "O vencedor foi"
 
-            winnerImage.setAttribute("src", "imagens/draw.svg")
-        }
+        player2Wins++
+
+    } else {
+        textModal.innerHTML = "Empatou"
+
+        winnerImage.setAttribute("src", "imagens/draw.svg")
     }
 
+    document.getElementById("active").style.visibility = 'visible';
 
+    round = 0;
+    turnOf = player1;
+    showPlayerTurn()
+    updateScoreboard()
+}
+
+function updateScoreboard() {
+    const resultXis = document.getElementById("result-xis");
+    const resultBall = document.getElementById("result-ball");
+
+    resultXis.innerHTML = player1Wins;
+    resultBall.innerHTML = player2Wins;
+}
+
+function resetScoreboard() {
+    player1Wins = 0;
+    player2Wins = 0;
+
+    updateScoreboard();
+}
+
+setFields();
+setRefreshBtn();
+updateScoreboard();
 
